f4dcda291b0358326e4b79f39b04971c8da5a55c
endrebjorgo <endrebjorgo@gmail.com>
2026-04-27 23:51:47 +0200

    Add SwapTrucks

--- src/operator.rs
+++ src/operator.rs
@@ -263,3 +263,34 @@ impl Operator for ScoochLaunchAndLanding {
         }
     }
 }
+
+
+pub struct SwapTrucks;
+
+impl Operator for SwapTrucks {
+    fn generate_neighborhood(&self, solution: &Solution) -> Vec<Solution> {
+        let mut neighborhood: Vec<Solution> = Vec::new();
+
+        for i in 1..(solution.truck_path.len() - 2) {
+            for j in i..(solution.truck_path.len() - 1) {
+                let mut new_truck_path = solution.truck_path.clone();
+                new_truck_path.swap(i, j);
+            
+                neighborhood.push(
+                    Solution::new(new_truck_path, solution.flights.clone())
+                );
+            }
+        }
+
+        return neighborhood;
+    }
+    fn get_random_neighbor(&self, solution: &Solution, rng: &mut dyn Rng) -> Option<Solution> {
+        let i = rng.random_range(1..(solution.truck_path.len() - 2));
+        let j = rng.random_range(i..(solution.truck_path.len() - 1));
+
+        let mut new_truck_path = solution.truck_path.clone();
+        new_truck_path.swap(i, j);
+    
+        return Some(Solution::new(new_truck_path, solution.flights.clone()));
+    }
+}
